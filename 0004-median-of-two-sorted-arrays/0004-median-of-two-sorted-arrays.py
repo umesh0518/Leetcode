a88be3f37bc7b038e8ec9f17fa281cbcb15bf57e
Umesh Yadav <41235691+umesh0518@users.noreply.github.com>
2026-02-25 01:42:21 -0500

    Time: 0 ms (100%), Space: 19.7 MB (13.73%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,22 +1,41 @@
-
+import statistics
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        combined = nums1 + nums2
-        combined.sort()
-
-        N = len(combined)
-
-        if N % 2 == 0:
-            l = int(N//2) -1 
-            r = l+ 1
-            sum = combined[l] + combined[r]
-            m = sum/2
-            return (m)
-
-
-        else:
-            c = int(((N + 1)/2)-1)
-            return combined[c]
-
-
         
+
+        A, B = nums1, nums2
+        total = len(A) + len(B)
+        half = total // 2
+
+        # so always smaller is A
+        if len(A) > len(B):
+            A, B = B, A
+        
+        left, right = 0, len(A)-1
+
+        while True:
+
+            pivotA = (left + right )//2
+            pivotB = half - pivotA - 2
+
+
+            Aleft = A[pivotA] if pivotA>=0 else float("-infinity")
+            Aright = A[pivotA + 1] if (pivotA + 1) < len(A) else float("infinity")
+
+            Bleft = B[pivotB] if pivotB >= 0 else float("-infinity")
+            Bright = B[pivotB + 1] if (pivotB + 1) < len(B) else float("infinity")
+
+
+            if Aleft <=Bright and Bleft <= Aright:
+                #means breaking pivot points are correct
+
+                if total % 2: 
+                    #odd case so just return the minimum between Aright and Bright
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+                
+            elif Aleft > Bright:
+                #means left of A was still bigger, 
+                right = pivotA - 1
+            else:
+                left = pivotA + 1
